c140325834eedf99303aa00fa90ed38ae3d8b3b6
charles.pax@gmail.com <Charles Edward Pax>
2025-05-22 11:59:32 -0400

    void script test if the system is live of installed

--- scripts/void.sh
+++ scripts/void.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-echo "exiting..."
+# TODO test if this script is being run in the live edition of
+# void or on an installed system. Throw an error if live.
+# Try  'cat /proc/cmdline | grep VOID_LIVE'. The live image will
+# return a line while an installed system will not.
 
-exit
+if grep -q "VOID_LIVE" "/proc/cmdline"; then
+	echo "we are live!"
+else
+	echo "this is not live"
+fi
+
+# Exit this script
+echo "exiting..."
+exit              # nothing below this line will be executed
+
+
+
 
 # Void Linux System Setup Notes
 # 
