e37941e9b8085191a6ec8cf7da57faab8ab80162
charles.pax@gmail.com <Charles Edward Pax>
2025-06-02 17:58:28 -0400

    made the suckless git directory configurable

--- scripts/install/suckless_install.sh
+++ scripts/install/suckless_install.sh
@@ -2,31 +2,36 @@
 
 # Install suckless utilties
 
-mkdir -p ~/gits/
+GIT_FOLDER=$HOME/gits    # Modify this to your needs
+
+echo "cloning to $GIT_FOLDER"
+mkdir -p $GIT_FOLDER
 
 #TODO make my own git repositories for suckless tools
-cd ~/gits
+cd $GIT_FOLDER
+echo "current location: `pwd`"
+
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
 #git clone https://git.suckless.org/surf
 
-cd ~/gits/dwm/
+cd $GIT_FOLDER/dwm/
 make clean
 make
 sudo make install
 
-cd ~/gits/dmenu/
+cd $GIT_FOLDER/dmenu/
 make clean
 make
 sudo make install
 
-cd ~/gits/st/
+cd $GIT_FOLDER/st/
 make clean
 make
 sudo make install
 
-#cd ~/gits/surf/
+#cd $GIT_FOLDER/surf/
 #make clean
 #make
 #sudo make install
