1c544e8845369923362c0c465822f8319a8a0805
charles.pax@gmail.com <Charles Edward Pax>
2025-06-03 06:16:36 -0400

    added slock to suckless install script. Script cleanup

--- scripts/install/suckless_install.sh
+++ scripts/install/suckless_install.sh
@@ -9,28 +9,37 @@ mkdir -p $GIT_FOLDER
 
 #TODO make my own git repositories for suckless tools
 cd $GIT_FOLDER
-echo "current location: `pwd`"
 
+# dwm -- Window Manager
 git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/dmenu
-git clone https://git.suckless.org/st
-#git clone https://git.suckless.org/surf
-
 cd $GIT_FOLDER/dwm/
 make clean
 make
 sudo make install
 
+# dmenu -- Application Launcher
+git clone https://git.suckless.org/dmenu
 cd $GIT_FOLDER/dmenu/
 make clean
 make
 sudo make install
 
+# slock -- Display Locker
+git clone https://git.suckless.org/slock
+cd $GIT_FOLDER/slock/
+make clean
+make
+sudo make install
+
+# wt -- Terminal Emulator
+git clone https://git.suckless.org/st
 cd $GIT_FOLDER/st/
 make clean
 make
 sudo make install
 
+# surf -- Web Browser
+#git clone https://git.suckless.org/surf
 #cd $GIT_FOLDER/surf/
 #make clean
 #make
